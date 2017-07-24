5e610b09804ccfc62abe3e32c9f9174d5670373a
xuliang <xuliang_1827@163.com>
2017-07-24 17:34:33 +0800

    learn netty

--- src/com/demon/netty/package-info.java
+++ src/com/demon/netty/package-info.java
@@ -1,9 +1,10 @@
 /**
- * Netty权威指南  page 96
+ * Netty权威指南  page 99
  * 
  * 基础篇  走进Java NIO
  * 第一章  Java I/O 演进之路
  * 第二章  NIO 入门
  * 第三章  Netty 入门
+ * 第四章 TCP 粘包/拆包问题的解决
  */
 package com.demon.netty;
