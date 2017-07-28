4a42e41e73b19b6aed420625cf95e4b2d7dace97
xuliang <xuliang_1827@163.com>
2017-07-28 17:47:36 +0800

    learn netty

--- src/com/demon/netty/package-info.java
+++ src/com/demon/netty/package-info.java
@@ -1,10 +1,13 @@
 /**
- * Netty权威指南  page 99
+ * Netty权威指南  page 144
  * 
  * 基础篇  走进Java NIO
  * 第一章  Java I/O 演进之路
  * 第二章  NIO 入门
  * 第三章  Netty 入门
  * 第四章 TCP 粘包/拆包问题的解决
+ * 第五章 分隔符和定长解码器的应用
+ * 第六章 编解码技术
+ * 第七章 Java 序列化
  */
 package com.demon.netty;
