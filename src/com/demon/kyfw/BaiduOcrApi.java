ae60ebbd81164ebb8fd5d3abc0971fbaf26a0a92
xuliang <xuliang_1827@163.com>
2018-09-11 09:16:21 +0800

    baidu ocr

---
+++ src/com/demon/kyfw/BaiduOcrApi.java
@@ -0,0 +1,175 @@
+package com.demon.kyfw;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * 百度文字、图片识别
+ * 
+ * @author xuliang
+ * @since 2018年9月7日 下午3:14:29
+ *
+ */
+public class BaiduOcrApi {
+    
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final String API_Key = "k2su7Ek2yD6HGqDCuh2OCUyU";
+    private final String Secret_Key = "bfyAWYyc9RdSc8ZoUegx2dPYkjHzscmI";
+    private final String Access_Token = "24.36291cea39307fdf0b0c318d4c4879ab.2592000.1538897841.282335-10639410";
+    
+    private CloseableHttpClient httpClient = HttpClients.createDefault();
+    
+    public void getAccessToken(){
+        String url = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id="+API_Key+"&client_secret="+Secret_Key;
+        String accessToken = get(url, null);
+        logger.info(accessToken);
+    }
+    
+    public void wordRecognition(String url){
+        //将图片文件转化为字节数组字符串，并对其进行Base64编码处理  
+        InputStream in = null;  
+        byte[] data = null;  
+        //读取图片字节数组  
+        try   
+        {  
+            in = new FileInputStream(new File(url));          
+            data = new byte[in.available()];  
+            in.read(data);  
+            in.close();  
+        }   
+        catch (IOException e)   
+        {  
+            e.printStackTrace();  
+        }  
+        //对字节数组Base64编码  
+        String imgData = Base64.getEncoder().encodeToString(data);//返回Base64编码过的字节数组字符串
+        logger.info("img data:{}", imgData);
+        
+        Map<String, String> params = new HashMap<>();
+        params.put("image", imgData);
+        String post = post("https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic?access_token="+Access_Token, params);
+        logger.info("word recognition result:{}", post);
+        if(post == null){
+            return ;
+        }
+        JSONObject json = JSON.parseObject(post);
+//        json.getJSONArray("words_result").getst
+    }
+    
+    private String get(String url, Map<String, String> params){
+        StringBuilder sb = new StringBuilder();
+        try{
+            URIBuilder builder = new URIBuilder(url);
+            if(params != null){
+                for(Map.Entry<String, String> entry: params.entrySet()){
+                    builder.setParameter(entry.getKey(), entry.getValue());
+                }
+            }
+            HttpGet httpGet = new HttpGet(builder.build());
+            CloseableHttpResponse resp = httpClient.execute(httpGet);
+            if(resp != null){
+                HttpEntity entity = resp.getEntity();
+                if(entity != null){
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
+                    String line = null;
+                    try {
+                        while ((line = reader.readLine()) != null) {
+                            sb.append(line + "\n");
+                        }
+                    } catch (IOException e) {
+                    } finally {
+                        try {
+                            reader.close();
+                        } catch (Exception e2) {
+                        }
+                        try {
+                            entity.getContent().close();
+                        } catch (IOException e) {
+                        }
+                    }
+                }
+            }
+            resp.close();
+        }catch (Exception e) {
+            logger.error("http get exception", e);
+        }
+        return sb.toString();
+    }
+    
+    public String post(String url, Map<String, String> params){
+        StringBuilder sb = new StringBuilder();
+        try{
+            List<NameValuePair> reqParams = new ArrayList<>();
+            if(params != null){
+                for(Map.Entry<String, String> entry: params.entrySet()){
+                    reqParams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+                }
+            }
+            UrlEncodedFormEntity encodedFormEntity = new UrlEncodedFormEntity(reqParams);
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            httpPost.setEntity(encodedFormEntity);
+            CloseableHttpResponse resp = httpClient.execute(httpPost);
+            if(resp != null){
+                HttpEntity entity = resp.getEntity();
+                if(entity != null){
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
+                    String line = null;
+                    try {
+                        while ((line = reader.readLine()) != null) {
+                            sb.append(line + "\n");
+                        }
+                    } catch (IOException e) {
+                    } finally {
+                        try {
+                            reader.close();
+                        } catch (Exception e2) {
+                        }
+                        try {
+                            entity.getContent().close();
+                        } catch (IOException e) {
+                        }
+                    }
+                }
+            }
+            resp.close();
+            
+        }catch (Exception e) {
+            logger.error("http post exception", e);
+        }
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) {
+//        new BaiduOcrApi().getAccessToken();
+        new BaiduOcrApi().wordRecognition("D:\\img\\0.jpg");
+    }
+    
+}
--- src/com/demon/kyfw/ImageOcr.java
+++ src/com/demon/kyfw/ImageOcr.java
@@ -1,17 +1,22 @@
 package com.demon.kyfw;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import com.demon.util.HttpClientWarper;
+import javax.imageio.ImageIO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * 图片识别，基于 baidu aip
+ * 图片下载，切分，合并
  * 
  * @author xuliang
  * @since 2018年1月6日 下午4:10:14
@@ -19,19 +24,9 @@ import com.demon.util.HttpClientWarper;
  */
 public class ImageOcr {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    
 	private String imageUrl = "https://kyfw.12306.cn/otn/passcodeNew/getPassCodeNew?module=login&rand=sjrand&0.3424303620122373";
-	
-	public void getImg() throws Exception{
-		String data = HttpClientWarper.sendGet2(imageUrl, "UTF-8");
-		System.out.println(data);
-		File imageFile = new File("D:\\code1.jpg");
-        //创建输出流  
-        FileOutputStream outStream = new FileOutputStream(imageFile);  
-        //写入数据  
-        outStream.write(data.getBytes());
-        //关闭输出流  
-        outStream.close(); 
-	}
 	
 	public void getImage() throws Exception{
 		//new一个URL对象  
@@ -46,9 +41,9 @@ public class ImageOcr {
         InputStream inStream = conn.getInputStream();  
         //得到图片的二进制数据，以二进制封装得到数据，具有通用性  
         byte[] data = readInputStream(inStream);  
-        System.out.println(new String(data));
+        logger.info("image data:{}", new String(data));
         //new一个文件对象用来保存图片，默认保存当前工程根目录  
-        File imageFile = new File("D:\\code.jpg");
+        File imageFile = new File("D:\\img\\code.jpg");
         //创建输出流  
         FileOutputStream outStream = new FileOutputStream(imageFile);  
         //写入数据  
@@ -57,7 +52,7 @@ public class ImageOcr {
         outStream.close();  
 	}
 	
-	public byte[] readInputStream(InputStream inStream) throws Exception{  
+	private byte[] readInputStream(InputStream inStream) throws Exception{  
         ByteArrayOutputStream outStream = new ByteArrayOutputStream();  
         //创建一个Buffer字符串  
         byte[] buffer = new byte[1024];  
@@ -74,10 +69,73 @@ public class ImageOcr {
         return outStream.toByteArray();  
     }  
 	
+	private static final String BASE_URL = "D:\\img\\";
+	public void splitImage(String imgUrl){
+	    try{
+    	    File file = new File(imgUrl);
+    	    FileInputStream fis = new FileInputStream(file);
+    	    BufferedImage image = ImageIO.read(fis);
+    	    
+    	    int imageHeight = image.getHeight();
+    	    int imageWidth = image.getWidth();
+    	    logger.info("image height:{} width:{}", imageHeight, imageWidth);
+    	    
+    	    BufferedImage[] imgs = new BufferedImage[9];
+    	    int index = 0;
+    	    
+    	    // 截取头部
+    	    int headHeight = 36;
+    	    int headWidth = imageWidth;
+    	    imgs[index] = new BufferedImage(headWidth / 2, headHeight, image.getType());
+    	    Graphics2D gr = imgs[index].createGraphics();
+    	    /*
+    	     * public abstract boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2,
+                                  int sx1, int sy1, int sx2, int sy2, ImageObserver observer)
+                img - 要绘制的指定图像。如果 img 为 null，则此方法不执行任何操作。
+                dx1 - 目标矩形第一个角的 x 坐标。
+                dy1 - 目标矩形第一个角的 y 坐标。
+                dx2 - 目标矩形第二个角的 x 坐标。
+                dy2 - 目标矩形第二个角的 y 坐标。
+                sx1 - 源矩形第一个角的 x 坐标。
+                sy1 - 源矩形第一个角的 y 坐标。
+                sx2 - 源矩形第二个角的 x 坐标。
+                sy2 - 源矩形第二个角的 y 坐标。
+                observer - 当缩放并转换了更多图像时要通知的对象。
+    	     */
+    	    gr.drawImage(image, 0, 0, headWidth, headHeight, 120, 0, headWidth, headHeight, null);
+    	    gr.dispose();
+    	    
+    	    // 截取图片
+    	    int rows = 2;
+    	    int cols = 4;
+    	    int chunkHeight = (imageHeight - headHeight) / rows;
+    	    int chunkWidth = imageWidth / cols;
+    	    
+    	    index++;
+    	    for(int x=0;x<rows;x++){
+    	        for(int y=0;y<cols;y++){
+    	            imgs[index] = new BufferedImage(chunkWidth, chunkHeight, image.getType());
+    	            Graphics2D gra = imgs[index].createGraphics();
+    	            gra.drawImage(image, 0, 0, chunkWidth, chunkHeight, 
+    	                    chunkWidth * y, chunkHeight * x + headHeight, chunkWidth * y + chunkWidth, chunkHeight * x + headHeight + chunkHeight, null);
+    	            gra.dispose();
+    	            index++;
+    	        }
+    	    }
+    	    
+    	    for(int i=0;i<imgs.length;i++){
+    	        ImageIO.write(imgs[i], "jpg", new File(BASE_URL + i + ".jpg"));
+    	    }
+    	    
+	    }catch (Exception e) {
+            logger.error("split image exception", e);
+        }
+	}
+	
 	public static void main(String[] args) throws Exception {
-		new ImageOcr().getImage();
-		System.out.println("--------------------------");
-		new ImageOcr().getImg();
+//		new ImageOcr().getImage();
+	    new ImageOcr().splitImage("D:\\img\\code.jpg");
+	    
 	}
 	
 }
